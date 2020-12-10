34d0b254bda6929141ad1dbb0f792b07a68f73a5
Piethur <Bramolijve@gmail.com>
2020-12-10 11:22:46 +0100

    Firebase geconnect Gradle update met firebase

--- OfficeSearcher3/app/src/main/java/com/example/officesearcher3/FirstFragment.kt
+++ OfficeSearcher3/app/src/main/java/com/example/officesearcher3/FirstFragment.kt
@@ -12,9 +12,7 @@ import androidx.navigation.fragment.findNavController
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class FirstFragment : Fragment() {
-//voor eerste commit
-    //voor testen github desktop\
-    //nog een keer testen
+
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
